a72aed0f69d01b89ac4a5c015b78117c82807164
Chiedo <chiedo@chie.do>
2016-01-30 11:40:42 -0500

    Basic test

---
+++ test.rb
@@ -0,0 +1,18 @@
+require "./lib/stl_parser.rb"
+puts "======================Testing Ascii=============================="
+
+parsed = STLParser.new
+parsed.process(Dir.pwd+'/ascii.stl')
+puts 'Volume:' + parsed.volume.to_s
+puts 'X dimensions:' + parsed.x_dimensions.to_s
+puts 'Y dimensions:' + parsed.y_dimensions.to_s
+puts 'Z dimensions:' + parsed.z_dimensions.to_s
+
+puts "======================Testing Binary=============================="
+parsed = STLParser.new
+parsed.process(Dir.pwd+'/binary.stl')
+
+puts 'Volume:' + parsed.volume.to_s
+puts 'X dimensions:' + parsed.x_dimensions.to_s
+puts 'Y dimensions:' + parsed.y_dimensions.to_s
+puts 'Z dimensions:' + parsed.z_dimensions.to_s
